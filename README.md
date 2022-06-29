4104d49accce2f6a7fd0e2c2d44ddf506238081d
ladianchad <qhrejddlvltm@gmail.com>
2022-06-29 19:28:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+[word_split](./word_split)
+https://www.acmicpc.net/problem/1251
---
+++ word_split/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
