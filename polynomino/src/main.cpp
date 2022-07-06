a83306da52a089928fd97e176f9cd7b8e8d69725
ladianchad <qhrejddlvltm@gmail.com>
2022-07-07 08:42:01 +0900

    edited polynomino

--- polynomino/src/main.cpp
+++ polynomino/src/main.cpp
@@ -1,57 +1,20 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <vector>
 
 using namespace std;
-
-string findPattern(string prev, int num)
-{
-  if(prev.length() > num){
-    return "";
-  }
-  if(prev.length() == num){
-    return prev;
-  }
-  string result;
-  result = findPattern(prev + "AAAA", num);
-  if(result == ""){
-    result = findPattern(prev + "BB", num);
-  }
-  return result;
-}
 
 int main(int argc, char const *argv[])
 {
   string board;
   getline(cin, board);
-  string result = "";
-  char prev;
-  int cnt = 0;
-  for(int i=0; i<board.length(); i++){
-    if(board[i] == 'X'){
-      cnt++;
-    } else if (prev == 'X'){
-      string pattern = findPattern("", cnt);
-      if(pattern == ""){
-        cout<< -1;
-        return 0; 
-      }
-      result += pattern + '.';
-      cnt = 0;
-    } else {
-      result += '.';
-    }
-    prev = board[i];
+  while(board.find("XXXX") != string::npos)
+  {
+    board.replace(board.find("XXXX"), 4, "AAAA");
   }
-  if(cnt){
-    string pattern = findPattern("", cnt);
-      if(pattern == ""){
-        cout<< -1;
-        return 0;
-      }
-      result += pattern;
+  while(board.find("XX") != string::npos)
+  {
+    board.replace(board.find("XX"), 2, "BB");
   }
-  cout<<result;
+  cout<< ((board.find('X') == string::npos)? board : "-1");
   return 0;
 }
